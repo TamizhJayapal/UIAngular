26924ff267eca8ab897e435bc7ce2f2ad8486bbe
tamil <ajtamizh96@gmail.com>
2018-08-24 16:37:04 +0530

    Routing Concept
    Add routing concept for service, sign in , sign up componet.

--- firstapp/src/app/app.module.ts
+++ firstapp/src/app/app.module.ts
@@ -2,19 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from './material_module';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { SiginComponent } from './sigin/sigin.component';
+import { SignupComponent } from './signup/signup.component';
+import { ServiceComponent } from './service/service.component';
+
+const appRoutes:Routes=[
+    {path:'home',component:HomeComponent},
+    {path:'signin',component:SiginComponent},
+    {path:'signup',component:SignupComponent},
+    {path:'service',component:ServiceComponent},
+    {path:'',redirectTo:'/home',pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SiginComponent,
+    SignupComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes,{enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- firstapp/src/app/connect.service.ts
+++ firstapp/src/app/connect.service.ts
@@ -4,7 +4,7 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ConnectService {
-  private _url:string = "apiData/empData.json";
+  private _url:string = "../apiData/empData.json";
   constructor(private _http:Http) { }
   gerData(){
     return this._http.get("https://jsonplaceholder.typicode.com/posts").map((response:Response)=>response.json()); 
